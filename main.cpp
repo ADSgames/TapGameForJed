45dcaa0279535c4bfe8c6aef59ec4e0f1a0e3147
Danny Van Stemp <danwardvs@adsgames.net>
2015-05-07 10:56:10 -0400

    Half of step timer and new images

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ BITMAP* buffer;
 BITMAP* coin;
 
 BITMAP* slave;
+BITMAP* mine;
+BITMAP* jedclone;
+BITMAP* forge;
 
 FONT *f1,*f2,*f3,*f4,*f5,*slabo_12;
 
@@ -18,6 +21,7 @@ int mpc = 1;
 int slaves;
 int slave_cost=100;
 int second_timer;
+int click;
 
 int mines;
 int mine_cost=500;
@@ -30,6 +34,8 @@ int forge_cost=1000;
 
 int villages;
 int village_cost=12500;
+
+int step;
 
 // FPS System
 volatile int ticks = 0;
@@ -94,6 +100,8 @@ void abort_on_error(const char *message){
 
 void update(){
 
+    step++;
+
     for( int i = 0; i <money_particle.size(); i++){
         //money_particle[i].lifespan++;
        money_particle[i].y--;
@@ -103,9 +111,10 @@ void update(){
     }
 
     second_timer++;
-    if(key[KEY_I])money+=10;
+    if(key[KEY_I])money+=1000;
 
-    if(key[KEY_A]){
+    if(key[KEY_A] && step>10){
+            step=0;
         if(money>=slave_cost){
             money-=slave_cost;
             mps+=10;
@@ -114,10 +123,11 @@ void update(){
         }
     }
 
-    if(key[KEY_S]){
+    if(key[KEY_S] && step>10){
+            step=0;
         if(money>=mine_cost){
             money-=mine_cost;
-            mps+=10;
+            mps+=25;
             mines++;
             mine_cost=mine_cost+(mine_cost/4);
         }
@@ -157,16 +167,20 @@ void update(){
      if(mouse_b & 1 && !mouse_pressed){
         mouse_pressed=true;
         money+=mpc;
+        click++;
         create_money_particle(random(200,600),random(150,500),mpc);
 
 
 
      }
-     if(!mouse_b & 1)
+     if(!mouse_b & 1){
         mouse_pressed=false;
+
+     }
     if(second_timer>60){
             second_timer=0;
         money+=mps;
+
         if(mps!=0)create_money_particle(random(200,600),random(150,500),mps);
     }
 }
@@ -179,24 +193,42 @@ void draw(){
     textprintf_ex( buffer, font, 20,40, makecol(0,0,0), -1, "JedCoins: %i",money);
     textprintf_ex( buffer, font, 20,60, makecol(0,0,0), -1, "JC/S: %i",mps);
     textprintf_ex( buffer, font, 20,70, makecol(0,0,0), -1, "JC/C: %i",mpc);
+    textprintf_ex( buffer, font, 20,80, makecol(0,0,0), -1, "Clicks: %i",click);
+
     textprintf_ex( buffer, font, 500,10, makecol(0,0,0), -1, "Slaves: %i",slaves);
     for( int i = 0; i <slaves; i++){
-        draw_sprite(buffer,slave,500+i*20,20);
+        draw_sprite(buffer,slave,500+i*20,15);
     }
 
 
-    textprintf_ex( buffer, font, 500,110, makecol(0,0,0), -1, "Jedcoin Mines: %i",mines);
-    textprintf_ex( buffer, font, 500,120, makecol(0,0,0), -1, "Jed Clones %i",jed_clones);
+    textprintf_ex( buffer, font, 500,50, makecol(0,0,0), -1, "Jedcoin Mines: %i",mines);
+
+    for( int i = 0; i <mines; i++){
+        draw_sprite(buffer,mine,500+i*27,55);
+    }
+
+
+    textprintf_ex( buffer, font, 500,85, makecol(0,0,0), -1, "Jed Clones %i",jed_clones);
+
+    for( int i = 0; i <jed_clones; i++){
+        draw_sprite(buffer,jedclone,500+i*15,90);
+    }
+
     textprintf_ex( buffer, font, 500,130, makecol(0,0,0), -1, "Jedcoin Forges %i",forges);
-    textprintf_ex( buffer, font, 500,140, makecol(0,0,0), -1, "Villages %i",villages);
+
+    for( int i = 0; i <forges; i++){
+        draw_sprite(buffer,forge,500+i*25,135);
+    }
+
+    textprintf_ex( buffer, font, 500,160, makecol(0,0,0), -1, "Villages %i",villages);
 
 
 
     textprintf_ex( buffer, font, 20,100, makecol(0,0,0), -1, "Press A to buy 1 slave: %i$ for 10 JC/S.",slave_cost);
-    textprintf_ex( buffer, font, 20,110, makecol(0,0,0), -1, "Press S to buy 1 coin mine: %i$ for 25 JC/S.",mine_cost);
+    textprintf_ex( buffer, font, 20,110, makecol(0,0,0), -1, "Press S to buy 1 Jedcon Mine: %i$ for 25 JC/S.",mine_cost);
     textprintf_ex( buffer, font, 20,120, makecol(0,0,0), -1, "Press D to buy 1 Jed clone: %i$ for %i JC/C.",jed_clone_cost,mpc*2);
-    textprintf_ex( buffer, font, 20,130, makecol(0,0,0), -1, "Press F to buy 1 Jedcoin Mine: %i$ for 100 JC/C.",forge_cost);
-    textprintf_ex( buffer, font, 20,140, makecol(0,0,0), -1, "Press G to buy 1 Jedcoin Mine: %i$ for 1000 JC/C.",village_cost);
+    textprintf_ex( buffer, font, 20,130, makecol(0,0,0), -1, "Press F to buy 1 Jedcoin Forge: %i$ for 100 JC/C.",forge_cost);
+    textprintf_ex( buffer, font, 20,140, makecol(0,0,0), -1, "Press G to buy 1 Jedcoin Village: %i$ for 1000 JC/C.",village_cost);
 
 
 
@@ -254,11 +286,21 @@ void setup(){
     LOCK_FUNCTION(close_button_handler);
     set_close_button_callback(close_button_handler);
 
-   if (!(coin = load_bitmap("images/coin.png", NULL)))
+    if (!(coin = load_bitmap("images/coin.png", NULL)))
      abort_on_error("Cannot find image images/coin.png\nPlease check your files and try again");
 
     if (!(slave = load_bitmap("images/slave.png", NULL)))
      abort_on_error("Cannot find image images/slaves.png\nPlease check your files and try again");
+
+    if (!(mine = load_bitmap("images/mine.png", NULL)))
+     abort_on_error("Cannot find image images/mine.png\nPlease check your files and try again");
+
+    if (!(jedclone = load_bitmap("images/jedclone.png", NULL)))
+     abort_on_error("Cannot find image images/jedclone.png\nPlease check your files and try again");
+
+    if (!(forge = load_bitmap("images/forge.png", NULL)))
+     abort_on_error("Cannot find image images/forge.png\nPlease check your files and try again");
+
 }
 
 
